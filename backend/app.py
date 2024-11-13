bf7afe36607f8ee9290acca275e6ce3b1375326b
LucasSBarros <101149312+LucasSBarros@users.noreply.github.com>
2024-11-13 10:05:30 -0300

    (build) alteração da porta

--- backend/app.py
+++ backend/app.py
@@ -49,7 +49,7 @@ def log_request_info():
 
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
     
     
