7b975f4821fd8a071d212d93b53cbfa4e2a3172a
LucasSBarros <101149312+LucasSBarros@users.noreply.github.com>
2024-11-13 09:52:00 -0300

    (fix) correção do build

--- backend/app.py
+++ backend/app.py
@@ -49,5 +49,7 @@ def log_request_info():
 
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
+    
+    
